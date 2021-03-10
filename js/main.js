a0c55d89637cc02277d1be43383f24f029ec44b6
Natalia Davydova <davydovanatalia063@gmail.com>
2021-03-09 19:48:50 -0800

    add section with popular products, tabs and slider there, add styles for section and script for slidet

--- js/main.js
+++ js/main.js
@@ -4,13 +4,23 @@ $(function () {
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
   });
-  $('.search__tabs-item').on('click', function (e) {
+  $('.tab').on('click', function (e) {
     e.preventDefault();
 
-    $('.search__tabs-item').removeClass('search__tabs-item--active');
-    $(".search__content-item").removeClass('search__content-item--active');
+    $('.tab').removeClass('tab--active');
+    $(".tabs-content").removeClass('tabs-content--active');
 
-    $(this).addClass('search__tabs-item--active');
-    $($(this).attr('href')).addClass('search__content-item--active');
-  })
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
+  });
+  $('.product-item__favourite').on('click', function () {
+    $(this).toggleClass('product-item__favourite--active');
+  });
+
+  $('.product-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow-right"></button>',
+  });
 });
